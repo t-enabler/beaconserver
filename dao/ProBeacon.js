f3135f04045751eaed7aeb4795e351daca7b0f15
Tou <tao@enabler.co.jp>
2021-02-05 10:16:12 +0900

    2021/02/05 BeaconAPI追加

--- dao/ProBeacon.js
+++ dao/ProBeacon.js
@@ -10,6 +10,33 @@ const getBeacondetail = function (b_uuid, b_major, b_minor) {
     return pros(sql);
 }
 
+const getAllBeacondetail = function () {
+    let sql = "SELECT * FROM beaconsever.beaconlist ;"
+    return pros(sql);
+}
+
+const insertnewbeacon = function (b_name, b_uuid, b_major, b_minor, b_txpower, b_lon, b_lat, b_alt, b_floor, b_note) {
+    let sql = "Insert into beaconsever.beaconlist(b_name,b_uuid,b_major,b_minor,b_txpower,b_lon,b_lat,b_alt,b_floor,b_note) VALUES (" + "'" + b_name + "','" + b_uuid + "','" + b_major + "','" + b_minor +
+        "','" + b_txpower + "','" + b_lon + "','" + b_lat + "','" + b_alt + "','" + b_floor + "','" + b_note + "');"
+    return pros(sql);
+}
+
+const getBeaconByid = function (b_id) {
+    let sql = "SELECT * FROM beaconsever.beaconlist where b_id=" + "'" + b_id + "';"
+    return pros(sql);
+}
+
+const deleteBeacon = function (b_id) {
+    let sql = "DELETE FROM beaconsever.beaconlist where b_id=" + "'" + b_id + "';"
+    return pros(sql);
+}
+
+const updateBeacon = function (b_id, b_name, b_uuid, b_major, b_minor, b_txpower, b_lon, b_lat, b_alt, b_floor, b_note) {
+    let sql = "UPDATE beaconsever.beaconlist set b_name = " + "'" + b_name + "',b_uuid = '" + b_uuid + "', b_major ='" + b_major + "', b_minor ='" + b_minor + "', b_txpower ='" + b_txpower +
+        "', b_lon ='" + b_lon + "', b_lat ='" + b_lat + "', b_alt ='" + b_alt + "', b_floor ='" + b_floor + "', b_note ='" + b_note + "' where b_id = '" + b_id + "';"
+    return pros(sql);
+}
+
 const pros = function (sql) {
     return new Promise((resolve, reject) => {
         client.connection.query(sql, (error, result, fields) => {
@@ -25,5 +52,10 @@ const pros = function (sql) {
 
 module.exports = {
     getBeaconuuidlist,
-    getBeacondetail
+    getBeacondetail,
+    getAllBeacondetail,
+    insertnewbeacon,
+    getBeaconByid,
+    deleteBeacon,
+    updateBeacon
 }
--- routes/index.js
+++ routes/index.js
@@ -19,20 +19,72 @@ router.get('/getbeaconuuidlist', async function (req, res, next) {
 
 router.post('/getbeacondetail', async function (req, res, next) {
     try {
-        const result = await beaconpro.getBeacondetail(req.body.data.beaconlist.values[0].uuid, req.body.data.beaconlist.values[0].mj, req.body.data.beaconlist.values[0].mn);
+        const result = await beaconpro.getBeacondetail(req.body.data.beaconlist[0].uuid, req.body.data.beaconlist[0].mj, req.body.data.beaconlist[0].mn);
         res.json({
-            "uid": req.body.data.beaconlist.uid,
+            "uid": req.body.data.uid,
             "lon": result[0].b_lon,
             "lat": result[0].b_lat,
             "alt": result[0].b_alt,
             "flr": result[0].b_floor,
-            "tim": req.body.data.beaconlist.tim,
-            "did": req.body.data.beaconlist.did
+            "tim": req.body.data.tim,
+            "did": req.body.data.did
         });
     } catch (e) {
         console.log(e);
         res.json({"status": "failed"});
     }
 });
+
+router.post('/insertnewbeacon', async function (req, res, next) {
+    try {
+        const result = await beaconpro.insertnewbeacon(req.body.data.b_name, req.body.data.b_uuid, req.body.data.b_major, req.body.data.b_minor, req.body.data.b_txpower, req.body.data.b_lon, req.body.data.b_lat, req.body.data.b_alt, req.body.data.b_floor, req.body.data.b_note);
+        res.json({"status": "success"});
+    } catch (e) {
+        console.log(e);
+        res.json({"status": "failed"});
+    }
+});
+
+router.post('/deleteBeacon', async function (req, res, next) {
+    try {
+        await beaconpro.deleteBeacon(req.body.data.b_id);
+        res.json({"status": "success"});
+    } catch (e) {
+        console.log(e);
+        res.json({"status": "failed"});
+    }
+});
+
+router.post('/updateBeacon', async function (req, res, next) {
+    try {
+        await beaconpro.updateBeacon(req.body.data.b_id,req.body.data.b_name, req.body.data.b_uuid, req.body.data.b_major, req.body.data.b_minor, req.body.data.b_txpower, req.body.data.b_lon, req.body.data.b_lat, req.body.data.b_alt, req.body.data.b_floor, req.body.data.b_note);
+        res.json({"status": "success"});
+    } catch (e) {
+        console.log(e);
+        res.json({"status": "failed"});
+    }
+});
+
+
+router.get('/getAllBeacondetail', async function (req, res, next) {
+    try {
+        const result = await beaconpro.getAllBeacondetail();
+        res.send(result);
+    } catch (e) {
+        console.log(e);
+        res.json({"status": "failed"});
+    }
+});
+
+router.get('/getBeaconByid/:Bid', async function (req, res, next) {
+    try {
+        const result = await beaconpro.getBeaconByid(req.params.Bid);
+        res.send(result);
+    } catch (e) {
+        console.log(e);
+        res.json({"status": "failed"});
+    }
+});
+
 
 module.exports = router;
